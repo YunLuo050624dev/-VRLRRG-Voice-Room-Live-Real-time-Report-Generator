c20823e334cf74d9c13e62ba07c3627e606377f8
YunLuo050624dev <xuletong567@outlook.com>
2026-06-02 06:49:04 +0800

    docs: sync full Apache 2.0 license, full repo sync

--- Src/main.cpp
+++ Src/main.cpp
@@ -1,3 +1,8 @@
+/*
+Copyright 2026 YunLuo
+Licensed under the Apache License, Version 2.0
+http://www.apache.org/licenses/LICENSE-2.0
+*/
 #include <windows.h>
 #include <windowsx.h>
 #include <commctrl.h>
